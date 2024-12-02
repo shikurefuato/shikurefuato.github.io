151a15cbd47963eb62a295b85ea06baa4d2e2040
shikurefuato <daiki322.0101@gmail.com>
2024-12-03 06:19:08 +0900

    Add files via upload

---
+++ menu100loadks4.js
@@ -0,0 +1,38 @@
+function sonoiti4(){ 
+return new Promise((resolve) => {
+    setTimeout(() => {
+
+const showFirst = document.querySelector('.kakushitei4');
+
+const showAds = document.querySelector('.kierungo4');
+
+showAds.classList.remove("adsbygoogle");
+showAds.classList.remove("kierungo4");
+showFirst.classList.add("d-none");
+showFirst.classList.remove("kakushitei4");
+
+      resolve();
+    }, 100);
+  });
+
+} 
+
+async function main4() {
+    if (isPageLoaded) {
+        return;
+    }
+
+sap2ort.insertAdjacentHTML('afterend', '<p class="kakushitei4"><ins class="adsbygoogle kierungo4"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-5005673256143644"
+     data-ad-slot="1277027499"><!-- --></ins></p>');
+
+(adsbygoogle = window.adsbygoogle || []).push({});
+
+    await sonoiti4();
+
+main4();
+}
+
+
+main4();
